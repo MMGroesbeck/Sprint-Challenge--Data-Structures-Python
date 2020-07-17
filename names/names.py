1048185f1070624802051ebe0cdc9cca88dae22d
M Groesbeck <m.groesbeck@gmail.com>
2020-07-17 08:48:35 -0700

    Goal and stretch for names.py completed.

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+import collections
 
 start_time = time.time()
 
@@ -13,10 +14,22 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# same logic but list comprehension for time baseline:
+# duplicates = [i for i in names_1 for j in names_2 if i == j] # 1.63 seconds
+
+# Python has a built-in .difference() method on sets, so convert lists to sets and then back:
+# x = set(names_1)
+# y = set(names_2)
+# z = x.difference(y) # returns elements in x but not in y (i.e. elements only in x)
+# q = x.difference(z) # returns elements in x but not in z, e.g. elements in x and in y (duplicates)
+# duplicates = [i for i in q] #.0050 seconds
+
+# same logic as above, but list comprehension instead of variable assignment:
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +39,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# See above -- using set comparisons, which are built-in, resulted in ~ 99.7% reduction in runtime.
