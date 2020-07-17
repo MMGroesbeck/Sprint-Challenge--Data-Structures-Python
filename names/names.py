ebbdddfb9ff9e25cf0af2c4daeed96d205e91514
M Groesbeck <m.groesbeck@gmail.com>
2020-07-17 10:41:26 -0700

    Binary search tree version of names goal completed.

--- names/names.py
+++ names/names.py
@@ -1,6 +1,11 @@
 import time
 import collections
 
+import sys
+sys.path.append('C:\\Users\\mgroe\\Desktop\\lambdaGit\\DS\\week 2\\Sprint-Challenge--Data-Structures-Python\\names')
+
+from sorting_tree import BSTNode
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -17,20 +22,36 @@ duplicates = []  # Return the list of duplicates in this data structure
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
-#             duplicates.append(name_1)
+#             duplicates.append(name_1) 
+# Baseline time: 4.0 seconds
 
-# same logic but list comprehension for time baseline:
+# same logic but list comprehension:
 # duplicates = [i for i in names_1 for j in names_2 if i == j] # 1.63 seconds
 
-# Python has a built-in .difference() method on sets, so convert lists to sets and then back:
+# Use a binary search tree
+# Checks to see if each name is already in.
+
+# name_tree = BSTNode()
+
+# for n in names_1:
+#     if not name_tree.contains(n):
+#         name_tree.insert(n)
+
+# for m in names_2:
+#     if name_tree.contains(m):
+#         if m not in duplicates:
+#             duplicates.append(m)
+#     else:
+#         name_tree.insert(m)
+# 0.76 seconds, O(log n) time average
+
+# # Stretch version:
 # x = set(names_1)
 # y = set(names_2)
 # z = x.difference(y) # returns elements in x but not in y (i.e. elements only in x)
 # q = x.difference(z) # returns elements in x but not in z, e.g. elements in x and in y (duplicates)
 # duplicates = [i for i in q] #.0050 seconds
-
-# same logic as above, but list comprehension instead of variable assignment:
-duplicates = [i for i in set(names_1).difference(set(names_1).difference(set(names_2)))] #.0050 seconds
+# duplicates = [i for i in set(names_1).difference(set(names_1).difference(set(names_2)))] #.0050 seconds
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
